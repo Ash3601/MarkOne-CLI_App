23dd90ded7d7534319c1310e8fe393af5bdbcc62
Ash_360 <abbasiashin0@gmail.com>
2021-08-16 21:05:06 +0530

    Completed the application

---
+++ questions.js
@@ -0,0 +1,25 @@
+const questions = [
+  {
+    question: "What is my favorite dish?\t",
+    answer: "Matar Paneer",
+  },
+  {
+    question: "Where do I stay?\t",
+    answer: "Kota",
+  },
+  {
+    question: "What is the brand of my personal Laptop?\t",
+    answer: "MSI",
+  },
+  {
+    question: "Which anime I love currently?\t",
+    answer: "Demon Slayer",
+  },
+  {
+    question: "My year of Graduation?\t",
+    answer: "2020",
+  },
+];
+
+module.exports.questions = questions;
+// export default questions;
